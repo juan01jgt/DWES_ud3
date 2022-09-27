246335e62bdd6032b13627760fefcdd452430034
JuanGT01 <jgato@iesflorenciopintado.es>
2022-09-27 09:06:12 +0200

    ej2

---
+++ Ejercicio2.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @author Juan Garcia
+ * Almaceno tres números en variables y los escribo en pantalla de manera ordenada.
+ */
+
+
+
+echo "</br>Código <a href='https://github.com/juan01jgt/DWES_ud3/blob/main/Ejercicio2.php'>aqui</a>";
