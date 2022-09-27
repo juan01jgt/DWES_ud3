964737cd065e84b504ec472c1eec45ffdcff61cf
JuanGT01 <jgato@iesflorenciopintado.es>
2022-09-27 09:18:25 +0200

    fin clase

--- Ejercicio2.php
+++ Ejercicio2.php
@@ -1,9 +1,21 @@
 <?php
 /**
  * @author Juan Garcia
- * Almaceno tres números en variables y los escribo en pantalla de manera ordenada.
+ * Cargo en variables mes y año e indico el número de días del mes. Utilizo la estructura de control switch
  */
 
-
+$mes=1;
+$año=1;
+if ($año % 4) {
+}
+switch ($mes) {
+    case 'value':
+        # code...
+        break;
+    
+    default:
+        # code...
+        break;
+}
 
 echo "</br>Código <a href='https://github.com/juan01jgt/DWES_ud3/blob/main/Ejercicio2.php'>aqui</a>";
