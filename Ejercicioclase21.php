21cb1428db70cc152af3b75aac878068e6e66932
JuanGT01 <jgato@iesflorenciopintado.es>
2022-10-05 01:16:15 +0200

    clase

---
+++ Ejercicioclase21.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @author Juan Garcia
+ * Indexación de los ejercicios mediante un array.
+ */
+
+
+
+echo "</br><a target='_blank' href='https://github.com/juan01jgt/DWES_ud3/blob/main/Ejercicioclase21.php'>Código</a>";
---
+++ Ejercicioclase22.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Juan Garcia
+ * Crear un array con los alumnos de clase y permitir la selección aleatoria de uno de ellos. El resultado debe mostrar nombre y fotografía.
+ */
+
+$aAlumnos = array(
+    array( "nombre"=>"","foto"=>""),
+);
+
+
+echo "</br><a target='_blank' href='https://github.com/juan01jgt/DWES_ud3/blob/main/Ejercicioclase22.php'>Código</a>";
