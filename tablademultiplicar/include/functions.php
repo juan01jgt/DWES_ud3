098274cdee6fe81e079018f30687421a38b6c0a1
JuanGT01 <jgato@iesflorenciopintado.es>
2022-10-17 10:06:06 +0200

    errores corregidos

--- tablademultiplicar/include/functions.php
+++ tablademultiplicar/include/functions.php
@@ -2,7 +2,7 @@
 function existeCoordenada ($fila,$columna,$array) : bool{
     $Existe=false;
     foreach ($array as $key => $value) {
-        if (($key['f'] == $fila) and ($value['c'] == $columna)) {
+        if (($value['f'] == $fila) and ($value['c'] == $columna)) {
             $Existe = true;
         }
     }
--- tablademultiplicar/index.php
+++ tablademultiplicar/index.php
@@ -12,7 +12,7 @@ $numAciertos = 0;
 $iconoRespuesta = '';
 $claseRespuesta = '';
 
-if (isset($_POST('send'))) {
+if (isset($_POST['send'])) {
     $procesaFormulario = true;
     foreach ($_POST['num'] as $f => $v1) {
         foreach ($v1 as $c => $v2) {
@@ -29,7 +29,7 @@ else {
     do {
         $f = mt_rand(1,NUMTABLAS);
         $c = mt_rand(1,NUMTABLAS);
-    } while ($existeCoordenada($f,$c,$numerosAleatorios));
+    } while (existeCoordenada($f,$c,$numerosAleatorios));
     $numerosAleatorios[] = array('f'=>$f,'c'=>$c);
     $valoresActuales[$f][$c]='';
   }  
@@ -52,21 +52,21 @@ else {
             <tr>
                 <td class="cabecera">X</td>
                 <?php
-                for ($i=1; $i < NUMTABLAS; $i++) { 
+                for ($i=1; $i <= NUMTABLAS; $i++) { 
                     echo "<td class='cabecera'>$i</td>";
                 }
                 echo "</tr>";
 
-                for ($f=1; $f < NUMTABLAS; $f++) { 
+                for ($f=1; $f <= NUMTABLAS; $f++) { 
                     echo "<tr>";
-                    echo "<td class='cabecera'>$f</td>";
-                    for ($c=21; $c < NUMTABLAS; $c++) { 
+                    echo "<td class='cabecera'>".$f."</td>";
+                    for ($c=1; $c <= NUMTABLAS; $c++) { 
                         if (existecoordenada($f,$c,$numerosAleatorios)) {
-                            if ($procesaformulario) {
+                            if ($procesaFormulario) {
                                 $iconoRespuesta = $valoresActuales[$f][$c] == $f*$c ? '&#128512' : '&#128534';
                                 $claseRespuesta = $valoresActuales[$f][$c] == $f*$c ? 'acierto' : 'fallo';
                             }
-                            echo "<td><input class='$claseRespuesta' title=\"".$f.'X'.$c."\" type'text' name=num[".$f."][".$c."] value=\"".$valoresActuales[$f][$c]."\"/>".$iconoRespuesta."</td>";
+                            echo "<td><input class=\"".$claseRespuesta."\" title=\"".$f.'X'.$c."\" type\"text.\" name=num[".$f."][".$c."] value=\"".$valoresActuales[$f][$c]."\"/>".$iconoRespuesta."</td>";
                         }
                         else {
                             echo "<td>".$f*$c."</td>";
@@ -83,7 +83,7 @@ else {
         echo "Aciertos: ".$numAciertos;
         if ($numAciertos == NUMINPUTS) {
             echo "<br/> Enhorabuena. Test completado<br/>";
-            echo "<a href=\"". htmlspecialchars($_SERVER['PHP_SELF'])."\">Reiniciar";
+            echo "<a href=\"". htmlspecialchars($_SERVER['PHP_SELF'])."\">Reiniciar</a>";
         }
     }
     ?>
